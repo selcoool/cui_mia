c98aa0039eb9a3f18fbc2c9504dcbaa1246edef0
Minh Thanh <trmthanh220895@gmail.com>
2026-05-03 06:10:18 +0800

    ssa

--- kits/espnow/address.ino
+++ kits/espnow/address.ino
@@ -21,3 +21,29 @@ void loop() {
 // esp32 c3 super mini:   18:8B:0E:92:66:34
 
 // esp32 dev : 14:2B:2F:EC:17:B8
+
+
+
+
+// #include <WiFi.h>
+
+// void setup() {
+//   Serial.begin(115200);
+
+//   while (!Serial) {
+//     delay(10); // đợi Serial sẵn sàng
+//   }
+
+//   delay(1000);
+
+//   WiFi.mode(WIFI_STA);
+//   WiFi.disconnect();
+
+//   Serial.println("START...");
+//   Serial.print("MAC Address: ");
+//   Serial.println(WiFi.macAddress());
+// }
+
+// void loop() {
+//   // không cần làm gì thêm
+// }
